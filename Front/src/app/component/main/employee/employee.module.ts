37b48b059d88c09d4bc7d277f838d32b82d68499
Unknown <ttp.tee@hotmail.com>
2018-06-17 01:42:48 +0700

    Front update

--- Front/src/app/component/main/employee/employee.module.ts
+++ Front/src/app/component/main/employee/employee.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmployeeComponent } from './employee.component';
 import { EmployeeRoutes } from './employee.routing';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 @NgModule({
   imports: [
     CommonModule,
     EmployeeRoutes
   ],
-  declarations: [EmployeeComponent]
+  declarations: [EmployeeComponent,
+    CreateEmployeeComponent
+]
 })
 export class EmployeeModule { }
--- Front/src/app/component/main/employee/employee.routing.ts
+++ Front/src/app/component/main/employee/employee.routing.ts
@@ -1,9 +1,21 @@
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeComponent } from './employee.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 const routes: Routes = [
   { path: '',
-  component: EmployeeComponent },
+  component: EmployeeComponent ,
+  children: [
+    {
+      path: '',
+      redirectTo: 'create',
+      pathMatch: 'full'
+    },
+    {
+      path: 'create',
+      component: CreateEmployeeComponent
+    }
+  ]}
 ];
 
 export const EmployeeRoutes = RouterModule.forChild(routes);
