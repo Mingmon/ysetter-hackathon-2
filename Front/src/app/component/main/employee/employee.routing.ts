55418f100026d945946f479bef5af1320713a7dc
Nattanan Semathong <60010313@kmitl.ac.th>
2018-06-17 10:48:51 +0700

    Merge branch 'MingmonDev' into Dev

--- Front/src/app/component/main/employee/employee.routing.ts
+++ Front/src/app/component/main/employee/employee.routing.ts
@@ -1,9 +1,21 @@
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeComponent } from './employee.component';
+import { ShowEmployeeComponent } from './show-employee/show-employee.component';
 
 const routes: Routes = [
   { path: '',
-  component: EmployeeComponent },
+  component: EmployeeComponent,
+  children: [
+    {
+      path: '',
+      redirectTo: 'show',
+      pathMatch: 'full'
+    },
+    {
+      path: 'show',
+      component: ShowEmployeeComponent
+    }
+  ] },
 ];
 
 export const EmployeeRoutes = RouterModule.forChild(routes);
