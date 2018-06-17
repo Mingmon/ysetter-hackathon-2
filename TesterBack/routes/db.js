29ff5dd71534dff6fa9ce1667950633703e120e6
Planxnx <60010465@kmitl.ac.th>
2018-06-17 11:41:53 +0700

    Auto stash before merge of "BackEnd" and "origin/BackEnd"

--- TesterBack/routes/db.js
+++ TesterBack/routes/db.js
@@ -8,3 +8,20 @@ var connect = mongojs(databaseUrl, collections);
 module.exports = {
     connect: connect
 };
+
+/*
+var mongojs = require('mongojs');
+
+var databaseUrl = 'Planx';
+var empcollections = ['projectid', 'employeeData',];
+var procollections = ['projectid', 'projectData',];
+
+var employeDB = mongojs(databaseUrl, empcollections);
+var projectDB = mongojs(databaseUrl, procollections);
+
+module.exports = {
+    employeDB: employeDB,
+    projectDB: projectDB
+};
+
+*/
